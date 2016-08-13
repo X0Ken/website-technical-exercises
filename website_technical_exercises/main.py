b7565192478c70b236c85d7ff4c902737c5cb740
wanghongxu <wang19930902@gmail.com>
2016-08-13 23:49:04 +0800

    Add some ext and set javascript files to local
    Flask-Bootstrap
    Flask-Moment
    Flask-Script

--- website_technical_exercises/main.py
+++ website_technical_exercises/main.py
@@ -1,6 +1,35 @@
+from datetime import datetime
+
 from flask import Flask
+from flask import render_template
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
 app = Flask(__name__)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+app.extensions['bootstrap']['cdns']['bootstrap'] = app.extensions['bootstrap']['cdns']['local']
+app.extensions['bootstrap']['cdns']['jquery'] = app.extensions['bootstrap']['cdns']['local']
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('user.html', name='wang',
+                           current_time=datetime.utcnow())
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+if __name__ == '__main__':
+    manager.run()
